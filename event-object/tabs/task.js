80bf2c1494d0aa75621e711c6ec97f7a4a0c15e7
LKl <plat.elena2010@ya.ru>
2026-01-15 15:25:46 +0300

    Initial

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,6 +1,6 @@
 const tabs = Array.from(document.querySelectorAll('.tab'));
-let activeTab = document.querySelector('.tab_active');
-let activeIndex = tabs.indexOf(activeTab)
+const activeTab = document.querySelector('.tab_active');
+const activeIndex = tabs.indexOf(activeTab);
 const contents = Array.from(document.querySelectorAll('.tab__content'));
 
 
@@ -11,7 +11,7 @@ document.addEventListener('click', (e) => {
         t.classList.remove('tab_active'));
     tab.classList.add('tab_active');
     contents[activeIndex].classList.remove('tab__content_active');
-    contents[newIndex].classList.add('tab__content_active')
+    contents[newIndex].classList.add('tab__content_active');
     activeIndex = newIndex;
 });
 

a49e66219c207cacfb1343b6b496d89af3ec4dc9
LKl <plat.elena2010@ya.ru>
2026-02-03 17:09:30 +0300

    Second

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,50 +1,8 @@
 const modal = document.getElementById('subscribe-modal'); 
 const close = document.querySelector('.modal__close'); 
 
-function setCookie(name, value, props) {
 
-    props = props || {}
-
-    var exp = props.expires
-
-    if (typeof exp == "number" && exp) {
-
-        var d = new Date()
-
-        d.setTime(d.getTime() + exp*1000)
-
-        exp = props.expires = d
-
-    }
-
-    if(exp && exp.toUTCString) { props.expires = exp.toUTCString() }
-
-    value = encodeURIComponent(value)
-
-    var updatedCookie = name + "=" + value
-
-    for(var propName in props){
-
-        updatedCookie += "; " + propName
-
-        var propValue = props[propName]
-
-        if(propValue !== true){ updatedCookie += "=" + propValue }
-    }
-
-    document.cookie = updatedCookie
-
-}
-
-function getCookie(name) {
-
-    var matches = document.cookie.match(new RegExp(
-      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ))
-    return matches ? decodeURIComponent(matches[1]) : undefined
-}
-
-if(getCookie("closed") !== "true"){
+if(!document.cookie.includes('closed=true')) {
  setTimeout(() => { 
      modal.classList.add("modal_active"); 
 }, 2000); 
@@ -53,7 +11,7 @@ if(getCookie("closed") !== "true"){
 
 close.addEventListener("click", () => { 
      modal.classList.remove("modal_active"); 
-     setCookie("closed", "true", {expires: 3600});
+     document.cookie = "closed=true; max-age=3600; path=/";
 });
 
 
